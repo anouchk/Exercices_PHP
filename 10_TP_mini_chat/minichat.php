c67c81b90c6eae81457b5fdbc6f55bc0b46d7bdc
Ana <analutzky@gmail.com>
2017-10-20 10:20:53 +0200

    Ajout d'une date format français et stocke du pseudo dans un cookie

--- 10_TP_mini_chat/minichat.php
+++ 10_TP_mini_chat/minichat.php
@@ -1,3 +1,12 @@
+<?php
+	//mise en place du cookie
+	if (isset($_POST['pseudo'])) {
+		setcookie('pseudo', $_POST['pseudo'], time() + 365*24*3600, null, null, false, true);
+	} else {
+		echo "pas de pseudo gardé en mémoire";
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -6,14 +15,24 @@
 	</head>
 	<body>
 		<form action="minichat_post.php" method="post">
-			<p><label> Pseudo</label> : <input type="text" name="pseudo"></p>
-			<p><label> Message</label> : <input type="text" name="message"></p>
+			<p>
+				<label for "pseudo"> Pseudo</label> : <input type="text" name="pseudo" id="pseudo" value="<?php
+  				if (isset($_COOKIE['pseudo'])) {
+    				echo htmlspecialchars($_COOKIE['pseudo']);
+  				} else {
+  					echo " pseudo";
+  				}
+   			?>">
+  			</p>
+  			
+			<p><label for "message"> Message</label> : <input type="text" name="message"></p>
 			<p><input type="submit" ></p>
 		</form>	
 		<br/ >
 		<p><h1> LE SUPER MINI CHAT </h1></p>
 
 		<?php
+			
 			// Connexion à la base de données
 			try {
 			$bdd = new PDO('mysql:host=localhost;dbname=TP_minichat', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
@@ -22,13 +41,13 @@
 			}
 
 			// Récupération des 10 derniers messages
-			$reponse = $bdd->query('SELECT * FROM minichat ORDER BY ID DESC  LIMIT 10');
+			$reponse = $bdd->query('SELECT ID, Pseudo, Message, DATE_FORMAT(date_com, \'%d/%m/%Y à %Hh%imin%ss\') AS date_com_fr  FROM minichat ORDER BY ID DESC  LIMIT 10');
 
 			// Affichage de chaque message (toutes les données sont protégées par htmlspcialchars)
 			// on va faire une boucle qui s'exécute tant qu'il y de la donnée qu'on va récupérer
 			// fetch ça récupère une nouvelle ligne à chaque fois : à chaque tour de boucle, on aura le contenu d'une nouvelle ligne dans la table. On l'aura dans un array qui s'appelle $donnees.
 			while ($donnees = $reponse->fetch()) {
-				echo '<p><strong>' . htmlspecialchars($donnees['Pseudo']) . ' : </strong> ' . htmlspecialchars($donnees['Message']). '</p>';
+				echo '<p>[' . htmlspecialchars($donnees['date_com_fr']) . '] <strong>' . htmlspecialchars($donnees['Pseudo']) . ' : </strong> ' . htmlspecialchars($donnees['Message']). '</p>';
 			}
 
 			$reponse->closeCursor();
--- 10_TP_mini_chat/minichat_post.php
+++ 10_TP_mini_chat/minichat_post.php
@@ -1,19 +1,20 @@
+
 <?php
-// Connexion à la base de données
-try {
-	// où se trouve le serveur de la bdd mysql ; quel est le nom de la bdd qu'on va utiliser ; quel est le login ; quel est le mot de passe pour s'y connecter :
-	$bdd = new PDO('mysql:host=localhost;dbname=TP_minichat', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-} catch(Exception $e) {
-	die('Erreur : '.$e->getMessage());
-}
+	//mise en place du cookie
+	setcookie('pseudo', $_POST['pseudo'], time() + 365*24*3600, null, null, false, true);
 
+	// Connexion à la base de données
+	try {
+		// où se trouve le serveur de la bdd mysql ; quel est le nom de la bdd qu'on va utiliser ; quel est le login ; quel est le mot de passe pour s'y connecter :
+		$bdd = new PDO('mysql:host=localhost;dbname=TP_minichat', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	} catch(Exception $e) {
+		die('Erreur : '.$e->getMessage());
+	}
 
-// Effectuer ici la requête qui insère le message reçu avec $_POST dans la base de données 
-$requete = $bdd->prepare('INSERT INTO minichat(Pseudo,Message) VALUES(?,?)');
-$requete->execute(array($_POST['pseudo'], $_POST['message']));
+	// Insertion du message reçu avec $_POST dans la base de données 
+	$requete = $bdd->prepare('INSERT INTO minichat(Pseudo,Message, date_com) VALUES(?,?,?)');
+	$requete->execute(array($_POST['pseudo'], $_POST['message'],  date('Y-m-d H:i:s')));
 
-// Puis rediriger le visiteur vers la page du minichat.php comme ceci :
-
-header('Location: minichat.php');
-
+	// Puis rediriger le visiteur vers la page du minichat.php comme ceci :
+	header('Location: minichat.php');
 ?>
