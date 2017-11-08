446d70f743a75faab9127bd886bf22d0c6a26aa2
Ana <analutzky@gmail.com>
2017-11-08 15:45:47 +0100

    MVC

---
+++ 13_MVC/controleur/blog/commentaires.php
@@ -0,0 +1,16 @@
+<?php 
+ 
+// On demande les 10 derniers commentaires (modèle) 
+include_once('modele/blog/get_commentaires.php'); 
+$commentaires = get_commentaires(0, 10); 
+ 
+// On effectue du traitement sur les données (contrôleur) 
+// Ici, on doit surtout sécuriser l'affichage 
+foreach($commentaires as $cle => $commentaire) 
+{ 
+    $commentaires[$cle]['titre'] = htmlspecialchars($commentaire['titre']); 
+    $commentaires[$cle]['contenu'] = nl2br(htmlspecialchars($commentaire['contenu'])); 
+} 
+ 
+// On affiche la page (vue) 
+include_once('vue/blog/commentaires.php'); 
--- 13_MVC/vue/blog/commentaires.php
+++ 13_MVC/vue/blog/commentaires.php
@@ -5,7 +5,7 @@
 
         <title>Mon blog</title>
 
-    	<link href="style.css" rel="stylesheet" /> 
+    	<link href="vue/blog/style.css" rel="stylesheet" /> 
 
     </head>
 
@@ -15,50 +15,31 @@
 
         <h1>Mon super blog !</h1>
 
-        <a href="php?section=index">Retour à la liste des billets</a>
-
-     <?php
-
-		// foreach($billets as $billet) {
-		// je devrais écrire un foreach ($commentaires as $commentaire)	{ }
-
- 
-	?>
+        <a href="blog.php?section=index">Retour à la liste des billets</a>
 
 			<div class="news">		
 				<h3> 
-					<?php echo htmlspecialchars($billet['titre']) ; ?>
-					<em><?php echo htmlspecialchars($billet['date_creation_fr']) ; ?></em>
+					<?php echo $billet['titre'] ; ?>
+					<em><?php echo $billet['date_creation_fr'] ; ?></em>
 				</h3>
 
 				<p> 
-					<?php echo nl2br(htmlspecialchars($billet['contenu'])) ; 
-					// La fonction nl2br() permet de convertir les retours à la ligne en balises HTML<br />. C'est une fonction dont vous aurez sûrement besoin pour conserver facilement les retours à la ligne saisis dans les formulaires.
-					?> 
+					<?php echo $billet['contenu'] ; ?> 
 				</p>
 			 </div>
 
 			<h2> Commentaires </h2>
 
-			<form action="commentaires_post.php" method="post">
-				<p><label> Pseudo</label> : <input type="text" name="pseudo"></p>
-				<p><label> Message</label> : <input type="text" name="message"></p>
-				<input type ="hidden" name="id2_billet" value="<?php echo $_GET['billet']?>">
-				<p><input type="submit" ></p>
-			</form>	
-
-			<?php
-				
-			while ($donnees2 = $reponse2->fetch()) {
+		     <?php
+				foreach ($commentaires as $commentaire)	{
 			?>
 				<p>
-					<strong><?php echo htmlspecialchars($donnees2['auteur']) ; ?></strong>
-					Le <?php echo htmlspecialchars($donnees2['date_commentaire_fr']) ; ?>
+					<strong><?php echo $commentaire['auteur']) ; ?></strong>
+					Le <?php echo $commentaire['date_commentaire_fr']) ; ?>
 				</p>
-				<div><?php echo nl2br (htmlspecialchars($donnees2['commentaire'])) ; ?></div>
+				<div><?php echo $commentaire['commentaire'])) ; ?></div>
 			<?php	
 			} // fin de la boucle des commentaires 
-			$reponse2->closeCursor();
 			?>
     </body>
 </html>
