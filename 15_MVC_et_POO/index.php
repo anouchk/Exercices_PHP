3294cc2595eb1b352079e8986ac8e1266ea70c5b
Ana <analutzky@gmail.com>
2017-11-09 17:06:12 +0100

    séparation de bog.php en index.php et affichageAccueil.php

---
+++ 15_MVC_et_POO/index.php
@@ -0,0 +1,15 @@
+<?php
+// Récupération des données
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=TP_commentaires_blog;charset=utf8', 'root', 'root');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+$req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
+
+require('affichageAccueil.php');
+?>
