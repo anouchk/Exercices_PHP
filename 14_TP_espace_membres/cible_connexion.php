3ef823fe73cb9bf75036e7e5807c755aad68df03
anouchk <analutzky@gmail.com>
2018-02-09 15:04:40 +0100

    debug avec Saysa

--- 14_TP_espace_membres/cible_connexion.php
+++ 14_TP_espace_membres/cible_connexion.php
@@ -7,40 +7,23 @@ try {
 		die('Erreur : '.$e->getMessage());
 }
 
-// Récupération des identifiants entrés par l'utilisateur à l'inscription
-$req = $bdd->prepare('SELECT id FROM membres WHERE pseudo = :pseudo AND pass = :pass');
-
-// Initialisation des variables récupérant les identifiants entrés par l'utilisateur à la connexion
-$pass = $_POST['pass'];
+// Etape 1 : recupérer dans la bdd la ligne qui correspond au pseudo
+// Préparer la requête (:pseudo ça correspondra à $pseudo)
+$req = $bdd->prepare('SELECT id, pass, pseudo FROM membres WHERE pseudo = :pseudo');
+// Initialisation de la variable récupérant le pseudo entré par l'utilisateur à la connexion
 $pseudo = $_POST['pseudo'];
+// Exécuter la requête : ça exécute et ça nous retourne le nombre de lignes affectées
+$req->execute(array('pseudo' => $pseudo));
+// Fetch permet de récupérer le résultat de la requête et de le renvoyer sous une certaine forme (tableau par exemple)
+$resultat = $req->fetch(PDO::FETCH_ASSOC);
+// var_dump($resultat); die;
 
-password_verify($_POST['pass'], $pass)
-
-$req->execute(array(
-    'pseudo' => $pseudo,
-    'pass' => $pass));
-$resultat = $req->fetch();
-
-if ($resultat) 
-{
+// etape 2 : comparer le pass du hash avec celui entré par le formulaire de connexion
+if (password_verify($_POST['pass'], $resultat['pass'])) {
     session_start();
     $_SESSION['id'] = $resultat['id'];
     $_SESSION['pseudo'] = $pseudo;
     echo 'Vous êtes connecté !';
-} 
-else 
-{
+} else {
     echo 'Mauvais identifiant ou mot de passe !';
 }
-
-// if (!$resultat)
-// {
-//     echo 'Mauvais identifiant ou mot de passe !';
-// }
-// else
-// {
-//     session_start();
-//     $_SESSION['id'] = $resultat['id'];
-//     $_SESSION['pseudo'] = $pseudo;
-//     echo 'Vous êtes connecté !';
-// }
